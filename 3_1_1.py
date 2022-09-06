8d46f4ac18c1cc6d2b29146c8594b7abd22b841e
PavelKushner <Pkushner87@gmail.com>
2022-09-06 20:20:40 +0300

    new solution

--- 3_1_1.py
+++ 3_1_1.py
@@ -1,9 +1,5 @@
 # Пользователь вводит предложение, заменить все пробелы на '-' 2-мя способами.
 text = input()
-text_new = []
-for i in range(len(text)):
-    if text[i] == ' ':
-        text_new.append('-')
-    else:
-        text_new.append(text[i])
-print(*text_new, sep='')
+text_new = text.split()
+text = '-'.join(text_new)
+print(text)
