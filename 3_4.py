a761f4ed998fdc03a675f81b9bd9e10251a7466e
PavelKushner <Pkushner87@gmail.com>
2022-09-07 20:39:19 +0300

    fix the file

--- 3_4.py
+++ 3_4.py
@@ -1,11 +1,12 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных.
+number_1, number_2, number_3 = input(), input(), input()
 above_0 = 0
 below_0 = 0
-for i in range(3):
-    number = float(input())
-    if number > 0:
-        above_0 += 1
-    elif number < 0:
-        below_0 += 1
+above_0 += '-' not in number_1 and int(number_1) != 0
+above_0 += '-' not in number_2 and int(number_2) != 0
+above_0 += '-' not in number_3 and int(number_3) != 0
+below_0 += '-' in number_1
+below_0 += '-' in number_2
+below_0 += '-' in number_3
 print('положительных чисел =', above_0)
 print('отрицательных чисел =', below_0)
