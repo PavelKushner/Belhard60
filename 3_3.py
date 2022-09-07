beb198815a1430a2474887d6a9a3073c55b0bf80
PavelKushner <Pkushner87@gmail.com>
2022-09-07 20:03:26 +0300

    add new file

--- 3_3.py
+++ 3_3.py
@@ -1,1 +1,9 @@
 # Пользователь вводит Имя, Возраст и Город, сформировать приветственное сообщение путем формотирования 3-мя способами.
+name, age, town = input(), input(), input()
+print('Добрый день. Меня зовут {name}. Мне {age} лет. Я из города {town}.'.format(name=name, age=age, town=town))
+
+name, age, town = input(), input(), input()
+print(f'Добрый день. Меня зовут {name}. Мне {age} лет. Я из города {town}.')
+
+name, age, town = input(), int(input()), input()
+print('Добрый день. Меня зовут %s. Мне %d лет. Я из города %s.' %(name, age, town))
