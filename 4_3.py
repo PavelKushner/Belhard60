736cfe407f98fcac0fcb8aaa8ff312e00144bf72
PavelKushner <Pkushner87@gmail.com>
2022-09-15 16:54:57 +0300

    add new task

--- 4_3.py
+++ 4_3.py
@@ -1,3 +1,5 @@
 # *Заполнить словарь где ключами будут выступать числа от 0 до n, а значениями вложенный словарь
 # с ключами "name" и "email", а значения для этих ключей будут браться с клавиатуры.
-numbers = int(input())
+n = int(input())
+new_dict = {i:{'name': input('name ='), 'email': input('email =')} for i in range(1, n+1)}
+print(new_dict)
