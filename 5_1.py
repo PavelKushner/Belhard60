86cbc8be3440ac021fc391b0118c29849cbd4def
PavelKushner <Pkushner87@gmail.com>
2022-09-20 10:05:23 +0300

    new solution

--- 5_1.py
+++ 5_1.py
@@ -1,11 +1,13 @@
 # Ввести первые N чисел кратные M и больше К
+import random
+
 N = int(input('Введи колличество выводимых чисел:'))
 M = int(input('Введи число для проверки на кратность: '))
 K = int(input('Число, которое не должно превышать выводимое число:'))
 num = 0
 numbers = []
 while num != N:
-    number = int(input('Введи число:'))
+    number = random.randint(-1000, 1000)
     if number % M == 0 and number > K:
         numbers.append(number)
         num += 1
