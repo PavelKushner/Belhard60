76639175eda87dbc7683f28c810c5f280fe4dfb8
PavelKushner <Pkushner87@gmail.com>
2022-09-22 15:22:23 +0300

    add new solution

--- 6_1.py
+++ 6_1.py
@@ -1,5 +1,5 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
-def translate(dec_num):
+def translate_binary(dec_num):
     new_dijit = []
     while dec_num != 0:
         new_dijit.append(dec_num % 2)
@@ -7,15 +7,19 @@ def translate(dec_num):
     new_dijit = new_dijit[::-1]
     print('Число в двоичной системе:', *new_dijit, sep='')
 
+def translate_dec(binary_num):
     new_dec_num = 0
     c = 1
-    s = len(new_dijit)
-    for i in range(len(new_dijit)):
-        if new_dijit[i] != 0:
-            new_dec_num += (2*int(new_dijit[i]))**(s-c)
+    s = len(binary_num)
+    for i in range(s):
+        if binary_num[i] != 0:
+            new_dec_num += (2*int(binary_num[i]))**(s-c)
             c += 1
         else:
             c += 1
     print('Число в десятичной системе:', new_dec_num)
 
-translate(int(input()))
+print('Введи десятичное число:')
+translate_binary(int(input()))
+print('Введи двоичное число:')
+translate_dec(input())
