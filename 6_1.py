1b06977c1810d9f3fe6233b799e6957da1b1f62f
PavelKushner <Pkushner87@gmail.com>
2022-09-22 14:42:44 +0300

    add new task

---
+++ 6_1.py
@@ -0,0 +1,23 @@
+# Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
+def translate(dec_num):
+    new_dijit = []
+    while dec_num != 0:
+        new_dijit.append(dec_num % 2)
+        dec_num = dec_num // 2
+    new_dijit = new_dijit[::-1]
+    print(*new_dijit, sep='')
+
+    new_dec_num = 0
+    c = 1
+    s = len(new_dijit)
+    for i in range(len(new_dijit)):
+        if new_dijit[i] != 0:
+            new_dec_num += (2*int(new_dijit[i]))**(s-c)
+            print(int(2*int(new_dijit[i]))**(s-c))
+            c += 1
+        else:
+            c += 1
+    print(2**0)
+    print(new_dec_num)
+
+translate(int(input()))
