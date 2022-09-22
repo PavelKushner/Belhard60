830cc05fe950d31d2167a1fb94053fa40b281a4d
PavelKushner <Pkushner87@gmail.com>
2022-09-22 14:45:33 +0300

    add new solution

--- 6_1.py
+++ 6_1.py
@@ -5,7 +5,7 @@ def translate(dec_num):
         new_dijit.append(dec_num % 2)
         dec_num = dec_num // 2
     new_dijit = new_dijit[::-1]
-    print(*new_dijit, sep='')
+    print('Число в двоичной системе:', *new_dijit, sep='')
 
     new_dec_num = 0
     c = 1
@@ -13,11 +13,9 @@ def translate(dec_num):
     for i in range(len(new_dijit)):
         if new_dijit[i] != 0:
             new_dec_num += (2*int(new_dijit[i]))**(s-c)
-            print(int(2*int(new_dijit[i]))**(s-c))
             c += 1
         else:
             c += 1
-    print(2**0)
-    print(new_dec_num)
+    print('Число в десятичной системе:', new_dec_num)
 
 translate(int(input()))
