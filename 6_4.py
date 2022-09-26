3e1d62b8e38ae292a565aebe9776ac0614581e12
PavelKushner <Pkushner87@gmail.com>
2022-09-26 10:50:41 +0300

    add new solution

--- 6_4.py
+++ 6_4.py
@@ -4,12 +4,13 @@
 
 def func(grand_list):
     s = len(grand_list)
-    for i in range(s):
-        if type(grand_list[i]) != type(''):
-            del grand_list[i]
-    #return grand_list
+    while s != 0:
+        if type(grand_list[s-1]) != type(''):
+            del grand_list[s-1]
+            s -= 1
+        else:
+            s -= 1
     print(grand_list)
 
-#grand_list = [1, 2, 1, 1, 123]
-
-func([1, 2, 1, 1, 123])
+grand_list = [1, 2, '1', 1, 123, 'dg']
+func(grand_list)
